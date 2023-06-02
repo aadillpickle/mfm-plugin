62bcfffa906b761d0c93e576352f1a88a7fa1d64
Aadil Ali <aliaadil2002@gmail.com>
2023-06-02 13:17:26 -0700

    inshallah

--- lc.py
+++ lc.py
@@ -1,20 +1,29 @@
 import os
-from langchain.vectorstores import Chroma
+from langchain.vectorstores import Pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
-
+import pinecone
 from dotenv import load_dotenv
 
 load_dotenv()
-persist_directory = os.path.join(os.getcwd(), "db")
-embeddings = OpenAIEmbeddings()
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+PINECONE_ENV = os.getenv("PINECONE_ENV")
 
-db = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
+pinecone.init(
+    api_key=PINECONE_API_KEY, 
+    environment=PINECONE_ENV  
+)
+
+embeddings = OpenAIEmbeddings()
+index_name = "mfm-embddings"
+
+docsearch = Pinecone.from_existing_index(index_name, embeddings)
 
 def get_relevant_info_from_question(query):
     relevant_info = []
-    docs = db.similarity_search_with_score(query, k=5)
+    docs = docsearch.similarity_search_with_score(query, k=5)
 
     for doc in docs:
+        print(doc[1])
         relevant_info.append({
                 "snippet": doc[0].page_content,
                 "podcast_title": doc[0].metadata["title"],
